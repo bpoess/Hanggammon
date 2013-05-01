dc662520f47642ed8ff4c36118d775a910eb6179
Bernhard Poess <bernhardpoess@gmail.com>
2013-04-30 22:01:35 -0700

    Make history window wider and prepend

--- static/js/history.js
+++ static/js/history.js
@@ -3,6 +3,6 @@ function history_add(msg)
    var hist = gapi.hangout.getLocalParticipant().person.displayName;
    hist += " " + msg + "<br>";
    var global_hist = gapi.hangout.data.getValue("history");
-   global_hist += hist;
-   gapi.hangout.data.setValue("history", global_hist);
+   hist += global_hist;
+   gapi.hangout.data.setValue("history", hist);
 }
