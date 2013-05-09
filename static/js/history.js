b90018cc4bc7105e24bfeb8f0c41630932fbd04c
Baran Nohutcuoglu <baran@nohutcuoglu.com>
2013-05-08 19:48:02 -0700

    I cannot write js

--- static/js/history.js
+++ static/js/history.js
@@ -2,7 +2,11 @@ function history_add(msg)
 {
    var hist = gapi.hangout.getLocalParticipant().person.displayName;
    var currentdate = new Date();
-   var datetime = currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds();
+   var seconds = currentdate.getSeconds();
+   if (seconds === "0") {
+      seconds = "00"
+   }
+   var datetime = currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + seconds;
    hist += "[" + datetime + "] " + msg + "<br>";
    var global_hist = gapi.hangout.data.getValue("history");
    hist += global_hist;
