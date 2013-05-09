d6e183635137748d6186ee38662cd3dc666952d8
Baran Nohutcuoglu <baran@nohutcuoglu.com>
2013-05-08 19:44:55 -0700

    formatting

--- static/js/history.js
+++ static/js/history.js
@@ -2,8 +2,8 @@ function history_add(msg)
 {
    var hist = gapi.hangout.getLocalParticipant().person.displayName;
    var currentdate = new Date();
-   var datetime = "@ " + currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds();
-   hist += " " + msg + datetime + "<br>";
+   var datetime = currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds();
+   hist += "[" + datetime + "] " + msg + "<br>";
    var global_hist = gapi.hangout.data.getValue("history");
    hist += global_hist;
    gapi.hangout.data.setValue("history", hist);
