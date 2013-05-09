ef49169e210d29580ffb499dc2fb543bdc47f7cf
Baran Nohutcuoglu <baran@nohutcuoglu.com>
2013-05-08 20:10:47 -0700

    better way of adding zeroes

--- static/js/history.js
+++ static/js/history.js
@@ -2,11 +2,20 @@ function history_add(msg)
 {
    // first part of history is the timestamp
    var currentdate = new Date();
+
+   var hours = currentdate.getHours();
+   if (hours < 10)
+      hours = "0" + hours
+
    var seconds = currentdate.getSeconds();
-   if (seconds == "0") {
-      seconds = "00"
-   }
-   var datetime = currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + seconds;
+   if (seconds < 10)
+      seconds = "0" + seconds
+
+   var minutes = currentdate.getMinutes();
+   if (minutes < 10)
+      minutes = "0" + minutes
+
+   var datetime = hours + ":" + minutes + ":" + seconds;
    var hist = "[" + datetime + "]"
 
    // append user name
