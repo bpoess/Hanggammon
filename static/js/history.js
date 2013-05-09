b507efe2c7a484a9528ef0d20a45f129226741de
Baran Nohutcuoglu <baran@nohutcuoglu.com>
2013-05-08 20:05:34 -0700

    debugger helps, fixed for real

--- static/js/history.js
+++ static/js/history.js
@@ -2,18 +2,18 @@ function history_add(msg)
 {
    // first part of history is the timestamp
    var currentdate = new Date();
-   var datetime = currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + seconds;
    var seconds = currentdate.getSeconds();
    if (seconds == "0") {
       seconds = "00"
    }
+   var datetime = currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + seconds;
    var hist = "[" + datetime + "]"
 
    // append user name
    hist += " " + gapi.hangout.getLocalParticipant().person.displayName;
 
    // finally the actual message
-   hist += msg + "<br>";
+   hist += " " + msg + "<br>";
 
    var global_hist = gapi.hangout.data.getValue("history");
    hist += global_hist;
