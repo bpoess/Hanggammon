440956ab6a2ad0d1f3c9a6f70ba657f9f656d084
Mark Pariente <markpariente@gmail.com>
2013-05-08 20:12:36 -0700

    Fix rest of game state

--- static/js/gameState.js
+++ static/js/gameState.js
@@ -2,16 +2,44 @@
 var gameState = [];
 
 // Board definitions
-var numSlotsPerBoard = 24;
+var numPiecesPerBoard = 15;
 var numBoards = 2;
 var numPlayers = 4;
 var numDice = 2;
 
 // Team definitions
-var team = [];
-team[0] = {pieceChar : '0'};
-team[1] = {pieceChar : '1'};
 var numTeams = team.length;
+
+// State definitions
+pieceState = {
+   IN_SLOT_0  : 0,
+   IN_SLOT_1  : 1,
+   IN_SLOT_2  : 2,
+   IN_SLOT_3  : 3,
+   IN_SLOT_4  : 4,
+   IN_SLOT_5  : 5,
+   IN_SLOT_6  : 6,
+   IN_SLOT_7  : 7,
+   IN_SLOT_8  : 8,
+   IN_SLOT_9  : 9,
+   IN_SLOT_10 : 10,
+   IN_SLOT_11 : 11,
+   IN_SLOT_12 : 12,
+   IN_SLOT_13 : 13,
+   IN_SLOT_14 : 14,
+   IN_SLOT_15 : 15,
+   IN_SLOT_16 : 16,
+   IN_SLOT_17 : 17,
+   IN_SLOT_18 : 18,
+   IN_SLOT_19 : 19,
+   IN_SLOT_20 : 20,
+   IN_SLOT_21 : 21,
+   IN_SLOT_22 : 22,
+   IN_SLOT_23 : 23,
+   MOVING     : 24,
+   HIT        : 25,
+   PICKED_UP  : 26
+}
 
 // Return the state key for a piece on the given board with the given team
 function getPieceKeyOnBoard(boardId, teamId, pieceId)
@@ -19,54 +47,54 @@ function getPieceKeyOnBoard(boardId, teamId, pieceId)
    return 'board' + boardId + '_team' + teamId + '_piece' _ pieceId;
 }
 
-// Return the state key for a given slot in the given board
-function getSlotKeyOnBoard(boardId, slotId)
-{
-   return 'board' + boardId + '_slot' + slotId;
-}
-
-// Return the key for the collected pieces of a given team on the given board
-function getTeamCollectedKeyOnBoard(boardId, teamId)
-{
-   return 'board' + boardId + '_team' + teamId + '_collected';
-}
-
-// Return the key for the currently "hit" pieces of a team on the given board
-function getTeamHitKeyOnBoard(boardId, teamId)
-{
-   return 'board' + boardId + '_team' + teamId + '_hit';
-}
-
 // Initialize the given board to be empty
 function clearBoard(boardId)
 {
-   for (var slot = 0; slot < numSlotsPerBoard; slot++) {
-      gameState[getSlotKeyOnBoard(boardId, slot)] = '';
-   }
-
    for (var team = 0; team < numTeams; team++) {
-      gameState[getTeamCollectedKeyOnBoard(boardId, team)] = '';
-      gameState[getTeamHitKeyOnBoard(boardId, team)] = '';
+      for (var piece = 0; piece < numPiecesPerBoard; piece++) {
+         gameState[getPieceKeyOnBoard(boardId, team, piece)] = '';
+      }
    }
-}
-
-// Repeat the given string multiple times
-String.prototype.repeat = function(num)
-{
-    return new Array(num + 1).join(this);
 }
 
 // Add standard piece configuration to a board
-function addStandardPiecesToBoard(boardId)
+function movePiecesToStandardLocations(boardId)
 {
-   gameState[getSlotKeyOnBoard(boardId, 0)] = team[0].pieceChar.repeat(2);
-   gameState[getSlotKeyOnBoard(boardId, 5)] = team[1].pieceChar.repeat(5);
-   gameState[getSlotKeyOnBoard(boardId, 7)] = team[1].pieceChar.repeat(3);
-   gameState[getSlotKeyOnBoard(boardId, 11)] = team[0].pieceChar.repeat(5);
-   gameState[getSlotKeyOnBoard(boardId, 12)] = team[1].pieceChar.repeat(5);
-   gameState[getSlotKeyOnBoard(boardId, 16)] = team[0].pieceChar.repeat(3);
-   gameState[getSlotKeyOnBoard(boardId, 18)] = team[0].pieceChar.repeat(5);
-   gameState[getSlotKeyOnBoard(boardId, 23)] = team[1].pieceChar.repeat(2);
+   // Team 0
+   gameState[getPieceKeyOnBoard(boardId, 0, 0)] = IN_SLOT_0.toString();
+   gameState[getPieceKeyOnBoard(boardId, 0, 1)] = IN_SLOT_0.toString();
+   gameState[getPieceKeyOnBoard(boardId, 0, 2)] = IN_SLOT_11.toString();
+   gameState[getPieceKeyOnBoard(boardId, 0, 3)] = IN_SLOT_11.toString();
+   gameState[getPieceKeyOnBoard(boardId, 0, 4)] = IN_SLOT_11.toString();
+   gameState[getPieceKeyOnBoard(boardId, 0, 5)] = IN_SLOT_11.toString();
+   gameState[getPieceKeyOnBoard(boardId, 0, 6)] = IN_SLOT_11.toString();
+   gameState[getPieceKeyOnBoard(boardId, 0, 7)] = IN_SLOT_16.toString();
+   gameState[getPieceKeyOnBoard(boardId, 0, 8)] = IN_SLOT_16.toString();
+   gameState[getPieceKeyOnBoard(boardId, 0, 9)] = IN_SLOT_16.toString();
+   gameState[getPieceKeyOnBoard(boardId, 0, 10)] = IN_SLOT_18.toString();
+   gameState[getPieceKeyOnBoard(boardId, 0, 11)] = IN_SLOT_18.toString();
+   gameState[getPieceKeyOnBoard(boardId, 0, 12)] = IN_SLOT_18.toString();
+   gameState[getPieceKeyOnBoard(boardId, 0, 13)] = IN_SLOT_18.toString();
+   gameState[getPieceKeyOnBoard(boardId, 0, 14)] = IN_SLOT_18.toString();
+   gameState[getPieceKeyOnBoard(boardId, 0, 15)] = IN_SLOT_18.toString();
+
+   // Team 1
+   gameState[getPieceKeyOnBoard(boardId, 1, 0)] = IN_SLOT_23.toString();
+   gameState[getPieceKeyOnBoard(boardId, 1, 1)] = IN_SLOT_23.toString();
+   gameState[getPieceKeyOnBoard(boardId, 1, 2)] = IN_SLOT_12.toString();
+   gameState[getPieceKeyOnBoard(boardId, 1, 3)] = IN_SLOT_12.toString();
+   gameState[getPieceKeyOnBoard(boardId, 1, 4)] = IN_SLOT_12.toString();
+   gameState[getPieceKeyOnBoard(boardId, 1, 5)] = IN_SLOT_12.toString();
+   gameState[getPieceKeyOnBoard(boardId, 1, 6)] = IN_SLOT_12.toString();
+   gameState[getPieceKeyOnBoard(boardId, 1, 7)] = IN_SLOT_7.toString();
+   gameState[getPieceKeyOnBoard(boardId, 1, 8)] = IN_SLOT_7.toString();
+   gameState[getPieceKeyOnBoard(boardId, 1, 9)] = IN_SLOT_7.toString();
+   gameState[getPieceKeyOnBoard(boardId, 1, 10)] = IN_SLOT_5.toString();
+   gameState[getPieceKeyOnBoard(boardId, 1, 11)] = IN_SLOT_5.toString();
+   gameState[getPieceKeyOnBoard(boardId, 1, 12)] = IN_SLOT_5.toString();
+   gameState[getPieceKeyOnBoard(boardId, 1, 13)] = IN_SLOT_5.toString();
+   gameState[getPieceKeyOnBoard(boardId, 1, 14)] = IN_SLOT_5.toString();
+   gameState[getPieceKeyOnBoard(boardId, 1, 15)] = IN_SLOT_5.toString();
 }
 
 // Return state key for player name
@@ -146,7 +174,7 @@ function resetGameState()
 {
    for (var board = 0; board < numBoards; board++) {
       clearBoard(board);
-      addStandardPiecesToBoard(board);
+      movePiecesToStandardLocations(board);
    }
 
    clearPlayers();
@@ -173,19 +201,15 @@ function gameStateToString()
 {
    var returnStr = '';
 
-   //for (var i = 0; i < numBoards; i++) {
-      //for (var j = 0; j < numSlotsPerBoard; j++) {
-         //returnStr += getSlotKeyOnBoard(i, j) + ' = "' +
-                      //gameState[getSlotKeyOnBoard(i, j)] + '"\n';
-      //}
-
-      //for (var j = 0; j < numTeams; j++) {
-         //returnStr += getTeamCollectedKeyOnBoard(i,j) + ' = "' +
-                      //gameState[getTeamCollectedKeyOnBoard(i,j)] + '"\n';
-         //returnStr += getTeamHitKeyOnBoard(i,j) + ' = "' +
-                      //gameState[getTeamHitKeyOnBoard(i,j)] + '"\n';
-      //}
-   //}
+   for (var board = 0; board < numBoards; board++) {
+      for (var team = 0; team < numTeams; team++) {
+         for (var piece = 0; piece < numPiecesPerBoard; piece++) {
+            returnStr += getPieceKeyOnBoard(board, team, piece) + ' = "' +
+                         gameState[getPieceKeyOnBoard(board, team, piece)] +
+                         '"\n';
+         }
+      }
+   }
 
    for (var i = 0; i < numPlayers; i++) {
       returnStr += getPlayerNameKey(i) + ' = "' +
